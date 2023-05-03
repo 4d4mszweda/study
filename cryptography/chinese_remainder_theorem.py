9b77cbada5b928406f4d5e6fa8919130fb38a25f
Adam <maxsuspl@gmail.com>
2023-05-03 19:39:42 +0200

    paste functions from older file

---
+++ cryptography/chinese_remainder_theorem.py
@@ -0,0 +1,42 @@
+def main():
+    data = insertText()
+
+def nwd(a, b):
+    if b > 0:
+        return nwd(b, a%b)
+    return a
+
+def extendedEuklides(a, b):
+    c = 0
+    x1, y2 = 1, 1
+    x2, y1 = 0, 0
+    while(a * b != 0):
+        if(a >= b):
+            c = int(a / b)
+            a %= b
+            x1 -= x2 * c
+            y1 -= y2 * c
+        else:
+            c = int(b / a)
+            b %= a
+            x2 -= x1 * c
+            y2 -= y1 * c
+    if(a > 0):
+        x = x1
+        y = y1
+    elif(b > 0):
+        x = x2
+        y = y2
+    return nwd(a, b), x, y
+
+def insertText():
+    text = []
+    while True:
+        try:
+            text.append( input() )
+        except:
+            break
+    return text
+
+if __name__ == "__main__":
+    main()
