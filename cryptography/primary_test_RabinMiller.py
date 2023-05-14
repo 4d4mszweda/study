2f45170694333d0c659eab60336339112ce947d8
Adam <maxsuspl@gmail.com>
2023-05-14 18:59:06 +0200

    first

--- cryptography/primary_test_RabinMiller.py
+++ cryptography/primary_test_RabinMiller.py
@@ -3,7 +3,17 @@ import math
 def main():
     n = int(input())
 
+def testRabinMiller(n, a = 0):
+    if(a == 0):
+        a = "COS"               #change later
+    
+    end = math.log(n , 2)
+    k = 10                      #change k later
+    for num in range(2, end):
+        if(n == k ** num):
+            return False
 
+    return False
 
 if __name__ == "__main__":
     main()
