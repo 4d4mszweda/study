7983910b18007cb14233369bdf64d8fb1efa19d6
Adam <maxsuspl@gmail.com>
2023-05-29 21:42:43 +0200

    Rabin miller rdy

--- cryptography/primary_test_RabinMiller.py
+++ cryptography/primary_test_RabinMiller.py
@@ -2,46 +2,78 @@ import math
 import random
 
 def main():
-    primary = testRabinMiller(280001, 105532)
+    data = insertText()
+    data = data[0].split()
+    primary = testRabinMiller(int(data[0]), int(data[2]), int(data[1]))
     if(primary):
-        print("Pierwsza")
+        print("Liczba jest prawdopodobnie pierwsza")
     else:
-        print("Złożona")
+        czynniki = sorted(czynniki_pierwsze(int(data[0])))
+        print("Liczba złożona: ", end='')
+        for cz in czynniki:
+            print(cz, end='')
+            if(cz != czynniki[-1]):
+                print("", end=' * ')
 
-def testRabinMiller(n, a = 0):
+def testRabinMiller(n, s, k):
+    random.seed(s)
+    random_number = random.randint(2, n - 1)
 
-    if(a == 0):
-        a = random.randint(1, n)
-    
-    end = int(math.log(n , 2))
-
-    k = 10                      #change k later
-    for num in range(2, end):
-        if(n == k ** num):
-            return False
-
-    m = (n - 1)/(2 ** k)
-
-    if(nwd(a, n) != 1):
+    if(n % 2 == 0 or n < 2):
         return False
-    x = (a ** m) % n
-    if(x == 1):
+    elif(n == 3):
         return True
-    temp = []
-    if(x != 1):
-        for num in range(1, k + 1):
-            temp.append((x ** (2 ** num)) % n)
-        temp_x = temp[0]
-        for next in temp:
-            if(next == 1):
-                if(temp_x != -1):
-                    return False
-                if(temp_x == -1):
-                    return True
-            temp_x = next
-        return False
 
-def millerRabinoHelper(n):
+    s1, t = millerRabinHelper(n)
+
+    random.seed(s)
+    random_number = random.randint(2, n - 1)
+    for next in range(k):
+        euk = moduloAmplification(random_number, s1, n)
+        if(euk != 1):
+            x = 0
+            while(euk != (n - 1)):
+                if(x == t - 1):
+                    return False
+                else:
+                    x += 1
+                    euk = (euk ** 2)% n
+    return True
+
+def czynniki_pierwsze(n):
+    czynniki = []
+    for i in range(2, n + 1):
+        while n % i == 0:
+            czynniki.append(i)
+            n //= i
+        if n == 1:
+            break
+    return czynniki
+
+def insertText():
+    text = []
+    while True:
+        try:
+            text.append( input() )
+        except:
+            break
+    return text
+
+def moduloAmplification(a, b, m):
+    result = 1
+    A = [a]
+    B = [b]
+    while(b != 0):
+        a = (a ** 2) % m
+        A.append(a)
+        b = b // 2
+        B.append(b)
+    for eb, ea in zip(B, A):
+        if(eb % 2 != 0):
+            result *= ea
+    return result % m
+
+def millerRabinHelper(n):
     x = n - 1
     tmp = 0
     while(x % 2 == 0):
