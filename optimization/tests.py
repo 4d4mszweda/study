bafce09716c2af0b05964edbc2e82c1556de4fd8
Adam Szweda <maxsuspl@gmail.com>
2024-03-10 14:31:41 +0100

    display fn for vertex cover

--- optimization/tests.py
+++ optimization/tests.py
@@ -1,9 +1,7 @@
 import unittest
 from szukanie_cyklu import force_method, multiplication_method
-from vertex_cover import vertex_cover
 
 class TestSzukanieCyklu(unittest.TestCase):
-
     def test_force_method(self):
         matrix = [[0, 1, 0],[1, 0, 1],[0, 1, 0]]
         matrix1 = [[0, 1, 0],[1, 0, 1],[1, 1, 0]]
@@ -16,18 +14,16 @@ class TestSzukanieCyklu(unittest.TestCase):
         self.assertEqual(multiplication_method(matrix), False)
         self.assertEqual(multiplication_method(matrix1), True)
 
-class TestVertexCover(unittest.TestCase):
-
+# class TestVertexCover(unittest.TestCase):
     # def test_vertex_cover(self):
         # graph = {(0, 1), (1, 2), (2, 0)}
         # cover = vertex_cover(graph)
         # self.assertEqual(len(cover), 2)
         # self.assertTrue(cover.issubset({0, 1, 2}))
-
-    def test_vertex_cover_presentation(self):
-        graph = {(0, 1), (1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (3, 6), (4, 5)}
-        cover = vertex_cover(graph)
-        print(cover)
+    # def test_vertex_cover_presentation(self):
+    #     graph = {(0, 1), (1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (3, 6), (4, 5)}
+    #     cover = vertex_cover(graph)
+    #     print(cover)
 
 if __name__ == '__main__':
     unittest.main()
--- optimization/vertex_cover.py
+++ optimization/vertex_cover.py
@@ -4,14 +4,23 @@ import random
 
 def vertex_cover_approximation(graph):
     cover = set()
-
     while graph.edges():
         u, v = random.choice(list(graph.edges()))
         cover.update([(u, v)])
         graph.remove_node(u)
         graph.remove_node(v)
-
     return cover
+
+def display_graph(graph, cover):
+    node_colors = []
+    unique_vertex_list = set([item for sublist in cover for item in sublist])
+    for node in graph.nodes():
+        if node in unique_vertex_list:
+            node_colors.append('r')
+        else:
+            node_colors.append('b')
+    nx.draw(G, with_labels=True, node_color=node_colors)
+    plt.show()
 
 G = nx.Graph()
 G.add_edges_from([(0, 1), (1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (3, 6), (4, 5)])
@@ -20,19 +29,5 @@ Ed = nx.Graph()
 Ed.add_edges_from([(0, 1), (1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (3, 6), (4, 5)])
 
 cover = vertex_cover_approximation(Ed)
-node_colors = []
 
-unique_vertex_list = set([item for sublist in cover for item in sublist])
-print(unique_vertex_list)
-
-for node in G.nodes():
-    if node in unique_vertex_list:
-        node_colors.append('r')
-    else:
-        node_colors.append('b')
-
-
-nx.draw(G, with_labels=True, node_color=node_colors)
-plt.show()
-
-print(cover)
+display_graph(G, cover)
