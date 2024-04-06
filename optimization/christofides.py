0ce415bc4e4f2b8acaa591d7d0f5c32c89a23172
Adam <maxsuspl@gmail.com>
2024-04-06 22:24:49 +0200

    opt problem

---
+++ optimization/christofides.py
@@ -0,0 +1,3 @@
+# Zaimplementuj algorytm Christofidesa wyznaczający rozwiązanie problemu komiwojażera o sumie wag nie większej niż 1.5OPT (alg. 1.5-przyblizony).
+# Uwaga: konieczna weryfikacja warunku trójkąta - niezbędnego do faktora przybliżenia 3/2.
+
