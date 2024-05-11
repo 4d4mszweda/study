c6df0fe6a71111e84e0541ca5e879c1dea6a8d4e
Adam Szweda <maxsuspl@gmail.com>
2024-05-11 21:12:48 +0200

    .

--- optimization/CPM.py
+++ optimization/CPM.py
@@ -4,6 +4,14 @@
 #  -> poda ścieżkę krytyczną, 
 #  -> poda harmonogram (wg najwcześniejszych czasów rozpoczęcia zadań 
 #  -> poda długość harmonogramu.
+class Task:
+    def __init__(self, name, duration, predecessors=None):
+        self.name = name
+        self.duration = duration
+        self.predecessors = predecessors if predecessors else []
+        self.earliest_start = None
+        self.latest_start = None
+
 
 
 def main():
