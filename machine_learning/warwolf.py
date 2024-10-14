f6e75c1e595026e984a868cf67702b87fa20bf8d
Adam <maxsuspl@gmail.com>
2024-10-14 19:16:51 +0200

    u76rd

---
+++ machine_learning/warwolf.py
@@ -0,0 +1,10 @@
+def main():
+    print("Hello world")
+    alfa = int(input("Podaj kąt alfa: "))
+    h = 100 # wysokość trebusza
+    v = 50 # prędkość m/s
+    # program ma obliczyć odległość d na którą poleci pocisk
+    return
+
+if __name__ == "__main__":
+    main()
